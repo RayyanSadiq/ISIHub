9fa73b9d369ba588a327172664826e63cc6b3f2d
Rayyan Sadiq <raysadik5@gmail.com>
2024-08-28 19:50:42 -0500

    first commit

---
+++ App.jsx
@@ -0,0 +1,16 @@
+ import Nav from "./components/Nav";
+
+
+const App = () => (
+  <main className="relative">
+    <Nav/> 
+    <section className="xl:padding-l wide:padding-r padding-b">
+    
+    </section>
+    
+ 
+ 
+  </main>
+)
+
+export default App;
